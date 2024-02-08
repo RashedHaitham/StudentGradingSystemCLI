f1e28871d0a0b7071fb3bf40a1f3b7f90b6903ba
rashed <rashedhaitham188@gmail.com>
2024-02-08 22:25:09 +0300

    finalV

--- src/main/java/org/example/View/View.java
+++ src/main/java/org/example/View/View.java
@@ -4,14 +4,10 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public abstract class View {
-
     protected Scanner input = new Scanner(System.in);
     public abstract String getDisplayOptions();
-
     public abstract void displayErrorMessage(String error);
-
     public abstract void displayEndingMessage(String message);
-
     public int getOptionValue(int min, int max) {
         int option = 0;
         boolean valid = false;
